a5e8d6de99707d29b03ec7b59b47d675dcc57f32
Guangchuang Yu <guangchuangyu@gmail.com>
2014-04-28 13:18:46 +0000

    add some visualization
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_14/madman/Rpacks/ChIPseeker@89574 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/plotAnno.R
+++ R/plotAnno.R
@@ -6,7 +6,6 @@
 ##' @param title plot title
 ##' @param xlab xlab
 ##' @param ylab ylab
-##' @param palette color set
 ##' @return bar plot that summarize genomic features of peaks
 ##' @importFrom plyr ldply
 ##' @importFrom ggplot2 ggplot
@@ -14,7 +13,6 @@
 ##' @importFrom ggplot2 geom_bar
 ##' @importFrom ggplot2 coord_flip
 ##' @importFrom ggplot2 theme_bw
-##' @importFrom ggplot2 scale_fill_brewer
 ##' @importFrom ggplot2 scale_x_continuous
 ##' @importFrom ggplot2 xlab
 ##' @importFrom ggplot2 ylab
@@ -31,8 +29,7 @@
 plotAnnoBar <- function(peakAnno,
                         title="Feature Distribution",
                         xlab="",
-                        ylab="Percentage(%)",
-                        palette=NULL) {
+                        ylab="Percentage(%)") {
     
     if ( class(peakAnno) == "data.frame" || class(peakAnno) == "GRanges" ) {
         anno.df <- getGenomicAnnoStat(peakAnno)
@@ -55,9 +52,8 @@ plotAnnoBar <- function(peakAnno,
     if (categoryColumn == 1) {
         p <- p + scale_x_continuous(breaks=NULL)
     }
-    if (is.null(palette) || is.na(palette))
-        return(p)
-    p <- p+scale_fill_brewer(palette=palette)
+
+    ## p <- p+scale_fill_manual(values=getCols(nrow(anno.df)))
     
     return(p)
 }
@@ -67,13 +63,12 @@ plotAnnoBar <- function(peakAnno,
 ##' 
 ##' @title plotAnnoPie
 ##' @param peakAnno peakAnno
-##' @param ndigit ndigit
-##' @param radius the radius of the pie
-##' @param explode the amount to "explode" the pie
-##' @param labelcex label font size
+##' @param ndigit number of digit to round
+##' @param cex label cex
+##' @param col color
+##' @param pie3D plot in 3D or not
 ##' @param ... extra parameter
 ##' @return pie plot of peak genomic feature annotation
-##' @importFrom plotrix pie3D
 ##' @examples
 ##' ## example not run
 ##' ## require(TxDb.Hsapiens.UCSC.hg19.knownGene)
@@ -86,23 +81,65 @@ plotAnnoBar <- function(peakAnno,
 ##' @author G Yu
 plotAnnoPie <- function(peakAnno,
                         ndigit=2,
-                        radius=0.8,
-                        explode=0.05,
-                        labelcex=1,
+                        cex=0.9,
+                        col=NA,
+                        pie3D=FALSE,
                         ...){
     
     anno.df <- getGenomicAnnoStat(peakAnno)
+    if (is.na(col)) {
+        col <- getCols(nrow(anno.df))
+    }
+    
+    if (pie3D)
+        annoPie3D(anno.df, ndigit=ndigit, cex=cex, col=col, ...)
+    
+    annoPie(anno.df, ndigit=ndigit, cex=cex, col=col, ...)
+ }
 
+##' @importFrom RColorBrewer brewer.pal
+##' @importFrom grDevices colorRampPalette
+annoPie <- function(anno.df, ndigit=2, cex=0.9, col=NA, ...) {
+    if ( ! all(c("Feature", "Frequency") %in% colnames(anno.df))) {
+        stop("check your input...")
+    }
+    
+    pie(anno.df$Frequency,
+        ## labels=paste(round(anno.df$Frequency/sum(anno.df$Frequency)*100, 2), "%", sep=""),
+        labels=paste(anno.df$Feature, " (",
+            round(anno.df$Frequency/sum(anno.df$Frequency)*100, ndigit),
+            "%)", sep=""),
+        cex=cex,
+        col=col,
+        ...
+        )
+    ## legend(legend=anno.df$Feature, fill=col, "topright")
+}
+
+## @param ndigit ndigit
+## @param radius the radius of the pie
+## @param explode the amount to "explode" the pie
+## @param labelcex label font size
+## @importFrom plotrix pie3D
+annoPie3D <- function(anno.df,
+                      ndigit=2,
+                      cex=1,
+                      ...){
+                  
+    ## anno.df <- getGenomicAnnoStat(peakAnno)
+
+    pkg <- "plotrix"
+    require(pkg, character.only=TRUE)
+    pie3D <- eval(parse(text="pie3D"))
     pie3D(anno.df$Frequency,
-          radius=radius,
-          explode=explode,
           labels=paste(
               anno.df$Feature,
               "(",
               paste(round(anno.df$Frequency, ndigit), "%", sep=""),
               ")",
               sep=""),
-          labelcex=labelcex,
+          labelcex=cex,
+          col=col,
           ...)
 }
 
@@ -110,8 +147,14 @@ getGenomicAnnoStat <- function(peakAnno) {
     if ( class(peakAnno) == "GRanges" )
         peakAnno <- as.data.frame(peakAnno)
     anno <- peakAnno$annotation
-    anno <- sub(" \\(.+", "", anno)
+    ## anno <- sub(" \\(.+", "", anno)
 
+    anno[grep("exon 1 of", anno)] <- "1st Exon"
+    anno[grep("intron 1 of", anno)] <- "1st Intron"
+    anno[grep("Exon \\(", anno)] <- "Other Exon"
+    anno[grep("Intron \\(", anno)] <- "Other Intron"
+    anno[grep("Downstream", anno)] <- "Downstream (<=3kb)"
+    
     ## count frequency
     anno.table <- table(anno)
     
@@ -119,6 +162,19 @@ getGenomicAnnoStat <- function(peakAnno) {
     anno.ratio <- anno.table/ sum(anno.table) * 100
     anno.df <- as.data.frame(anno.ratio)
     colnames(anno.df) <- c("Feature", "Frequency")
+    lvs <- c("Promoter (<=1kb)",
+             "Promoter (1-2kb)",
+             "Promoter (2-3kb)",
+             "5' UTR",
+             "3' UTR",
+             "1st Exon",
+             "Other Exon",
+             "1st Intron",
+             "Other Intron",
+             "Downstream (<=3kb)",
+             "Distal Intergenic")
+    anno.df$Feature <- factor(anno.df$Feature, levels=lvs[lvs %in% anno.df$Feature])
+    anno.df <- anno.df[order(anno.df$Feature),]
     return(anno.df)
 }
 
