c9031f6a319dd648c8b80abedc4c2f6a1e060a91
Guangchuang Yu <guangchuangyu@gmail.com>
2025-11-04 22:21:39 +0800

    fixed test issue

--- tests/testthat/test-txdb.R
+++ tests/testthat/test-txdb.R
@@ -6,7 +6,6 @@ library(yulab.utils)
 context("TXDB")
 
 test_that("Update txdb", {
-
     hg19_txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
     ChIPseeker:::.ChIPseekerEnv(hg19_txdb)
     expect_equal(ChIPseeker:::get_env_genome(), "hg19")
@@ -14,7 +13,6 @@ test_that("Update txdb", {
     hg38_txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
     ChIPseeker:::.ChIPseekerEnv(hg38_txdb)
     expect_equal(ChIPseeker:::get_env_genome(), "hg38")
-
 })
 
 
@@ -22,6 +20,20 @@ test_that("txdb", {
     txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
     ChIPseeker:::.ChIPseekerEnv(txdb)
     expect_equal(ChIPseeker:::IDType(txdb), "Entrez Gene ID")
-    expect_equal(ChIPseeker:::TXID2EG("70455"), "uc002qsd.4/1")
-    expect_equal(ChIPseeker:::TXID2EG("70455", geneIdOnly=TRUE), "1")
+    # expect_equal(ChIPseeker:::TXID2EG("70455"), "uc002qsd.4/1")
+    # expect_equal(ChIPseeker:::TXID2EG("70455", geneIdOnly=TRUE), "1")
+
+    if (packageVersion("TxDb.Hsapiens.UCSC.hg19.knownGene") > "3.22") {
+        expect_equal(
+            ChIPseeker:::TXID2EG("70455"),
+            "ENST00000487630.1_3/ENST00000487630.1_3"
+        )
+        expect_equal(
+            ChIPseeker:::TXID2EG("70455", geneIdOnly = TRUE),
+            "ENST00000487630.1_3"
+        )
+    } else {
+        expect_equal(ChIPseeker:::TXID2EG("70455"), "uc002qsd.4/1")
+        expect_equal(ChIPseeker:::TXID2EG("70455", geneIdOnly = TRUE), "1")
+    }
 })
